ba5c5c3532a6df37bec6f98a29ef9f3d3e957b6d
Jose Perez <abog.joseperez@gmail.com>
2025-12-10 14:15:37 -0400

    variables simples y buen uso de sintaxis

---
+++ tipos de datos/datos-simples.py
@@ -0,0 +1,12 @@
+"string"
+'string'
+
+
+'''string'''
+''' tus datos son:
+    nombre:jose
+    apellido: peez'''
+    
+    40 "int"
+    40.2 "float"
+    True False "Bool"
---
+++ variables/variales.py
@@ -0,0 +1,38 @@
+#como funciona las variables
+##nombre = "Maria "
+ 
+#bienvenida = "hola " +nombre + "Como estas?"
+
+#print(bienvenida)
+
+
+#como funciona las variables con numero,dando error,solo string no mezcla int
+#nombre = "55 "
+
+#nombre = 55
+
+#bienvenida = "hola " +nombre + "Como estas?"
+
+#print(bienvenida)
+
+# uso de f string
+#nombre = "jesus "
+ 
+#bienvenida = f"hola {nombre} Como estas?"
+
+#print(bienvenida)
+
+
+# uso de del para borrar una variable
+#nombre = "Jose"
+#del nombre 
+#bienvenida = f"hola  {nombre } Como estas?"
+
+
+#print( "Jose" in bienvenida)
+
+nombre = "Jose"
+bienvenida = f"hola  {nombre } Como estas?"
+
+
+print( "mario" in bienvenida)
