ebadf4f174848958acbd31ffaa8257f916c375b0
Jose Perez <abog.joseperez@gmail.com>
2025-12-11 08:20:27 -0400

    operador arismetico

---
+++ tipos de datos/operadores/op_arismeticos.py
@@ -0,0 +1,27 @@
+#suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 -5
+
+#multiplicacio  y division (* y /)
+multi = 12 * 5
+# la division devuelbe un dato float
+divi = 12 / 5
+
+# potenciacion (exponente) (**)
+exponente = 12**5
+
+#division baja (//)
+divicion_baja = 12//5 # vuelve un entero redondo a bajo
+
+#resto 0 modulo
+resto = 12 % 5
+
+tipo_de_dato = type(2.0) #type(dato) nos develve el tipo de dato
+
+
+
+print(resto)
+
+
+
+
