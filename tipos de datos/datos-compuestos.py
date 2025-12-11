45efeee12cfaf7f74796396d4db6105fba358479
Jose Perez <abog.joseperez@gmail.com>
2025-12-10 20:05:13 -0400

    Tipod de datos compuestos

---
+++ tipos de datos/datos-compuestos.py
@@ -0,0 +1,26 @@
+
+#arrays
+#las listas se pueden modificar
+#lista = [" Jose Perez ","@JosePerez-v8c6e",True,1.82]
+#print(lista[1])
+
+
+#las duplas no se pueden modificar
+#Duplas
+#lista = [" Jose Perez ","@JosePerez-v8c6e",True,1.82]
+#print(lista[1])
+
+#los conjuntos
+
+
+#diccionarios
+diccionario = {
+    'nombre' : "jose Perez",
+    'canal' : '@josePerez-v8c6e',
+    'esta_emocionado' : 'True',
+    'altura' : '1.82',
+    'dato_duplicado' : '@josePerez-v8c6e',
+}
+
+print(diccionario['nombre'])
+
