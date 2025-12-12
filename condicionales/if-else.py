c1a0c6da163bf00fbdf58f3016be983fbc4ee87a
Jose Perez <abog.joseperez@gmail.com>
2025-12-12 07:52:12 -0400

    operadores y condicionales

---
+++ condicionales/if-else.py
@@ -0,0 +1,29 @@
+#estructura
+#if condicion :
+  #  accion
+
+#if True :
+    #la accion se ejecuta
+    
+#if False :
+    #accion no se ejecuta    
+'''
+edad = 21
+    
+if edad >= 18:
+        print("puedes pasar")
+        
+else :
+    print("No puedes pasar")
+    '''
+    #ejemplo
+   
+contrasena_almacenada = "JosePerez"
+contrasena_escrita = "JosePerez"
+
+if contrasena_almacenada == contrasena_escrita :
+    print("INICIANDO SESION")
+else:
+    print("CONTRASENA EQUIVOCADA, INTENTE DE NUEVO")
+
+ 
---
+++ operadores/op_de_comparacion.py
@@ -0,0 +1,28 @@
+igual_que = 5 == 6
+
+distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5>= 6
+
+menor_o_igual = 5 <= 6
+
+a = 25
+b = 25
+c = 50
+
+resultado = a + b <= c
+
+#comparar usuarios
+
+usuario_de_base_de_datos = "Jose Perez"
+usuario_escrito = "jose Perez"
+
+contrasena_almacenada = "JosePerez"
+contrasena_escrita = "JosePerez"
+
+print(contrasena_almacenada== contrasena_escrita)
+ 
