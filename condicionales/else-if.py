f257e5a8743453df1fa4d844a648ccd412fade87
Jose Perez <abog.joseperez@gmail.com>
2025-12-12 21:01:10 -0400

    archivo  els if modificado

---
+++ condicionales/else-if.py
@@ -0,0 +1,10 @@
+
+ingreso_mensual = 10001
+
+if ingreso_mensual > 10000:
+    print("Esta bien en cualquier parte del mundo")
+elif ingreso_mensual > 1000:
+    print("Esta bien en latinoamerica")
+    
+else:
+    print("sos pobre")
