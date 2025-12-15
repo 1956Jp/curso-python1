b7f1e2199da439d9b14b2df2f92adc67c382cbf2
Jose Perez <abog.joseperez@gmail.com>
2025-12-15 16:09:52 -0400

    operadores

---
+++ operadores/operadores_logicos.py
@@ -0,0 +1,19 @@
+# and compara uno y otro
+
+Resultado = True & True =Devolver True
+Resultado2 = True & True = Devolver True
+Resultado3 = True & True = Devolver True
+Resultado4= True & True = Devolver True
+
+#or
+Resultado5= True | True = Devolver True
+Resultado6= True | True = Devolver True
+Resultado7= True | True = Devolver True
+Resultado8= True | True = Devolver True
+
+#not
+
+Resultado9= not True = Devolver True
+Resultado10= not True = Devolver True
+
+print(Resultado)
