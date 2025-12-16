880688dd4bad31d622b9b0e760ed70f29b43b936
Jose Perez <abog.joseperez@gmail.com>
2025-12-15 20:39:17 -0400

    condicionales comparadores y cadenas

---
+++ condicionales/stri_cadenas/cadena.py
@@ -0,0 +1,40 @@
+#DIR: DEVUELBE LA LISTA DE ATRIBUTOS VALIDOS DEL OBJETO PASADO
+
+#UPPER- combierte a mayuscula
+#LOWER- convierte a minusculas
+#CAPITALIZE- primera en mayuscula
+
+#FIND - metodo encuentra la primera aparicion del valor especificado, sino devuelve1
+#INDEX - metodo encuentra la primera aparicion del valor especificado, sino devuelbe una excepcion
+
+#ISNUMERIC - si es numerico devuelbe true
+#ISALPHA -se es alfa numerico devuelbe true
+
+#COUNT -devuelbe elnumero de ocurrencia de una sub cadena en la cadena dada
+#LEN -cuenta los caracteres de una cadena
+
+#ENDSWITH -verifica si una cadena comienza con
+#STARTSWITH -verifica si una cadena termina con
+
+#REPLACE -remplaza u valor por otro
+#SPLIT -separa por el parametro dado
+
+
+
+cadena1 = "Hola soy Dalton"
+
+cadena2 = "Viembenidos a Yaritagua"
+
+#print(dir(cadena1))
+#print(dir(4))
+#print(dir(["soadasoi"]))
+
+#resultado = "Hola mostro como andas".upper()
+#resultado = cadena1.lower() 
+resultado = cadena1.capitalize() 
+#resultado = cadena1.upper() 
+ 
+
+
+                                   
+print(resultado)
---
+++ operadores/op_logico.py
@@ -0,0 +1,20 @@
+# and compara uno y otro
+
+Resultado = True & True # Devolver True
+Resultado2 = False& True # Devolver True
+Resultado3 = True & False # Devolver True
+Resultado4= True & True #Devolver True
+
+#or
+Resultado5= True | True # Devolver True
+Resultado6= False | True # Devolver True
+Resultado7= True | False # Devolver True
+Resultado8= False | False # Devolver True
+
+#not
+
+Resultado9= not True # Devolver True
+Resultado10= not False # Devolver True
+
+print(Resultado4)
+
