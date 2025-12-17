3643b24589e21aacf349a5f602ba82ff2fef7f65
Jose Perez <abog.joseperez@gmail.com>
2025-12-16 21:52:56 -0400

    metodos de cadena.py

--- condicionales/stri_cadenas/cadena.py
+++ condicionales/stri_cadenas/cadena.py
@@ -8,6 +8,7 @@
 #INDEX - metodo encuentra la primera aparicion del valor especificado, sino devuelbe una excepcion
 
 #ISNUMERIC - si es numerico devuelbe true
+
 #ISALPHA -se es alfa numerico devuelbe true
 
 #COUNT -devuelbe elnumero de ocurrencia de una sub cadena en la cadena dada
@@ -23,18 +24,30 @@
 
 cadena1 = "Hola soy Dalton"
 
-cadena2 = "Viembenidos a Yaritagua"
-
+cadena2 = "1958"
+# Biemvenidos 2 a Yaritagua 1
 #print(dir(cadena1))
 #print(dir(4))
 #print(dir(["soadasoi"]))
 
 #resultado = "Hola mostro como andas".upper()
 #resultado = cadena1.lower() 
+mayuscula = cadena2.upper()
 #resultado = cadena1.capitalize() 
+letra_capital = cadena2.capitalize()
 #resultado = cadena1.upper() 
- 
+#buscar una cadena en otra cadena
+busqueda_find = cadena1.find("s")
 
 
+es_numerico = cadena2.isnumeric()
+es_alpha = cadena2.isalpha()
+#contamo las coincidencia en una cadena
+contar_coincidencias = cadena1.count("o")
+
+#contamos cuantos caracteres tiene una cadena
+contar_caracteres = len(cadena2)
                                    
-print(resultado)
+print(contar_caracteres)
+
+
