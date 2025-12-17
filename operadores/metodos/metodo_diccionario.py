9ca4e31637ffff32bd512476a97b51873b466358
Jose Perez <abog.joseperez@gmail.com>
2025-12-17 14:19:32 -0400

    metodos de listas y duplas

---
+++ operadores/metodos/metodo_diccionario.py
@@ -0,0 +1,9 @@
+diccionario = {
+    "nombre" : 'Jose',
+    "apellido" : 'Perez',
+    "subs" : 100000
+}
+#keys() -> devuelve la clave(tambiensirve para interar)
+clave = diccionario.keys()
+
+print(clave)
---
+++ operadores/metodos/metos_lista.py
@@ -0,0 +1,37 @@
+#APPEND: agrega un elemento a la lista
+#INSERT: agrega un elemento a la lista en el indice especificado
+
+#CLEAR: elimina todos los elementos de una lista
+#SORT: ordena una lista de forma ascendente o desendente
+
+#REVERSE: invierte los elementos de una lista
+#LIST: crea una lista (es una funcion)
+lista = list([34, 89, 32, False,True])
+#print(lista)
+
+
+#LENT: cuenta la cantidad de elementos de una lista
+cadena = "Hola"
+             #cadena.lent()
+resultado =  len(lista)                 
+              #len(cadena)   
+#print(resultado)
+#devuelve la cantidad de elementos de la lista
+cantidad_elementos = len(lista)
+#agregando elementos a la lista
+#lista.append("jajaja")
+#INSERT: agregar un elemento a la lista en el indice especificado
+#lista.insert(3,"toma_antonio")
+#EXTEND: agrega varios elementos a la lista
+#lista.extend(['zapato','cuchara',1, 2 ,3])
+#POP: elimina elementos de una lista, pide indices y devuelve valor
+#lista.pop(6)
+#REMOVE: remueve elementos de una lista, pide valor
+#lista.remove('cuchara')
+# si usamos el parametro reverse=True se reversa los elementos de la lista
+lista.sort()
+lista.reverse()
+#print(lista)
+print(lista)
+
+
