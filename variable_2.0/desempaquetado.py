836e905dd9674662f6b81468b92b12e4949b7781
Jose Perez <abog.joseperez@gmail.com>
2025-12-24 14:09:02 -0400

    modificacionsegunda, tuplas diccionarios conjuntos

---
+++ variable_2.0/desempaquetado.py
@@ -0,0 +1,11 @@
+
+#creando los datos
+datos_en_tuplas = ("Jose","Perez", 1000000)
+datos_en_listas = ["Jose","Perez", 1000000]
+
+
+# desempaquetado
+nombre, apellido, subcriptores = datos_en_tuplas
+
+#mostrando los resultados
+print(subcriptores)
---
+++ variable_2.0/tuplas.py
@@ -0,0 +1,12 @@
+
+#creando tuplas con tuple
+#tupla = tuple(["Jose","dato2"])
+
+#creando una tupla sin parentesis de multiples datos
+#tupla = "Jose","datos2"
+
+#creando una tupla sin paraentesis de un solo dato
+tupla = "Jose",
+
+print(tupla)
+
